dc3e3ed2859e3bbb5c25d9847cfa5e6e2c5afeee
Robert Sami <anonymized>
2025-05-15 20:50:31 -0400

    Add init guard to sentry in renderer process to silence breadcrumb warnings

--- apps/desktop/src/renderer/src/main.tsx
+++ apps/desktop/src/renderer/src/main.tsx
@@ -6,12 +6,14 @@ import * as Sentry from "@sentry/electron/renderer";
 import ReactDOM from "react-dom/client";
 import App from "./App.js";
 
-Sentry.init({
-  // Adds request headers and IP for users, for more info visit:
-  // https://docs.sentry.io/platforms/javascript/guides/electron/configuration/options/#sendDefaultPii
-  sendDefaultPii: true,
-  integrations: [Sentry.captureConsoleIntegration({ levels: ["error"] })],
-});
+if (process.env.NODE_ENV === "production") {
+  Sentry.init({
+    // Adds request headers and IP for users, for more info visit:
+    // https://docs.sentry.io/platforms/javascript/guides/electron/configuration/options/#sendDefaultPii
+    sendDefaultPii: true,
+    integrations: [Sentry.captureConsoleIntegration({ levels: ["error"] })],
+  });
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   // TODO: turn StrictMode back on once we figure out how to deal with streaming
