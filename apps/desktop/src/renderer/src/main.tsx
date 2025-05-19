9a218f56f3b48854fe31fe3cbf6a735901d3133e
Luke Tchang <ltchang@stanford.edu>
2025-05-19 16:37:04 -0700

    session replay

--- apps/desktop/src/renderer/src/main.tsx
+++ apps/desktop/src/renderer/src/main.tsx
@@ -10,8 +10,17 @@ if (process.env.NODE_ENV === "production") {
   Sentry.init({
     // Adds request headers and IP for users, for more info visit:
     // https://docs.sentry.io/platforms/javascript/guides/electron/configuration/options/#sendDefaultPii
+    dsn: "https://0959c176189c84d818acd95b7add26ac@o4509322414063616.ingest.us.sentry.io/4509322496180224",
     sendDefaultPii: true,
-    integrations: [Sentry.captureConsoleIntegration({ levels: ["error"] })],
+    integrations: [
+      Sentry.captureConsoleIntegration({ levels: ["error"] }),
+      Sentry.replayIntegration({
+        maskAllText: true,
+        blockAllMedia: true,
+      }),
+    ],
+    replaysSessionSampleRate: 1.0,
+    replaysOnErrorSampleRate: 1.0,
   });
 }
 
