e27d4dcdcea59f76c52e05c88f3dd62d540ff0f0
Luke Tchang <ltchang@stanford.edu>
2025-05-27 13:35:48 -0700

    extracting facts

--- apps/desktop/src/renderer/src/components/CellView.tsx
+++ apps/desktop/src/renderer/src/components/CellView.tsx
@@ -218,7 +218,7 @@ const PostprocessingSpinner: React.FC<{
 }> = ({ hasLogStep, hasCodeStep, isThinking }) => {
   return (
     <StatusIndicator
-      text="Extracting Timeline"
+      text="Extracting Facts"
       isVisible={isThinking && (hasLogStep || hasCodeStep)}
     />
   );
